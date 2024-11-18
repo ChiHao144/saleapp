64c77c2669487279eae82d1cd4e63c6deb2949c6
Chi Hao <haopc1404@gmail.com>
2024-11-18 22:25:36 +0700

    add data

---
+++ app/dao.py
@@ -0,0 +1,61 @@
+def load_categories():
+    return [{
+        "id": 1,
+        "name": "Mobile"
+    }, {
+        "id": 2,
+        "name": "Tablet"
+    }]
+
+
+def load_products():
+    return [{
+        "id": 1,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }, {
+        "id": 2,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 2
+    }, {
+        "id": 3,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }, {
+        "id": 4,
+        "name": "Galaxy Note 12 Pro",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 45000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }, {
+        "id": 4,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }, {
+        "id": 5,
+        "name": "Redmi Note 13",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 15000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }, {
+        "id": 6,
+        "name": "Xiaomi Poco a67 ",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 17000000,
+        "image": "https://cdnv2.tgdd.vn/mwg-static/common/News/1569924/9.jpg",
+        "category_id": 1
+    }]
